3b403ee41fc3ba9cbbfb7d8f0da06748f2052118
Alexandr Korolev <dowellkin@gmail.com>
2021-11-19 15:23:59 +0300

    namespace module upgrade

--- namespaces.js
+++ namespaces.js
@@ -1,34 +1,35 @@
 class Namespaces{
 	constructor(api){
 		this.api = api;
+		this.moduleBase = 'namespaces'
 	}
 
 	create(title){
-		return this.api.post('/namespaces', {
+		return this.api.post(`/${this.moduleBase}`, {
 			title
 		})
 	}
 
 	delete(id){
-		return this.api.delete(`/namespaces/${id}`)
+		return this.api.delete(`/${this.moduleBase}/${id}`)
 	}
 
 	join(namespace, {account, access, role}){
-		return this.api.post(`/namespaces/${namespace}/link`, {
+		return this.api.post(`/${this.moduleBase}/${namespace}/link`, {
 			account,
-			access: access ?? '0',
+			access: access ?? '1',
 			role: role ?? 'default'
 		})
 	}
 
 	link(namespace, account){
-		return this.api.post(`/namespaces/${namespace}/link`, {
+		return this.api.post(`/${this.moduleBase}/${namespace}/link`, {
 			account
 		})
 	}
 
 	list(depth = 10){
-		return this.api.get('/namespaces', {
+		return this.api.get(`/${this.moduleBase}`, {
 			params: {
 				depth
 			}
