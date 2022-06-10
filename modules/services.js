33efad8cb5eb8fd35c0f1a3293b2a350ac59cac1
va-ovsyanikov <va.ovsyanikov@gmail.com>
2022-06-10 16:19:06 +0300

    _update item  services

--- modules/services.js
+++ modules/services.js
@@ -1,66 +1,69 @@
-class Services{
-	constructor(api){
+class Services {
+	constructor(api) {
 		this.api = api;
 		this.moduleBase = 'services'
 	}
 
-	list(){
+	list() {
 		return this.api.get(`/${this.moduleBase}`)
 	}
 
-	get(id){
-		if(id === undefined){
+	get(id) {
+		if (id === undefined) {
 			throw 'id is undefined'
 		}
 		return this.api.get(`/${this.moduleBase}/${id}`)
 	}
 
-	up(id, instance_group_uuid, services_provider_uuid){
-		if(id === undefined){
+	up(id, instance_group_uuid, services_provider_uuid) {
+		if (id === undefined) {
 			throw 'id is undefined'
 		}
 		return this.api.post(`/${this.moduleBase}/${id}/up`, {
-    	"deploy_policies": {
-        [instance_group_uuid]: services_provider_uuid
+			"deploy_policies": {
+				[instance_group_uuid]: services_provider_uuid
 			}
 		})
 	}
 
-	down(uuid, body = {}){
+	down(uuid, body = {}) {
 		return this.api.post(`/${this.moduleBase}/${uuid}/down`, body)
 	}
 
-	_create(body){
+	_create(body) {
 		return this.api.put(`/${this.moduleBase}`, body)
 	}
+	_update(uuid, body) {
+		return this.api.patch(`/${this.moduleBase}/${uuid}`, body)
+	}
 
-	_testConfig(body){
+	_testConfig(body) {
 		return this.api.post(`/${this.moduleBase}`, body)
 	}
 
-	create({namespace, service}){
+	create({ namespace, service }) {
 		return this._create({
 			"namespace": namespace,
 			"service": service
 		})
 	}
 
-	testConfig({namespace, service}){
+	testConfig({ namespace, service }) {
 		return this._testConfig({
 			"namespace": namespace,
 			"service": service
 		})
 	}
 
-	delete(id){
+	delete(id) {
 		return this.api.delete(`/${this.moduleBase}/${id}`)
 	}
 
-	action({service, group, instance}, action, body = {}){
+	action({ service, group, instance }, action, body = {}) {
 		return this.api.post(`/${this.moduleBase}/${service}/${group}/${instance}/action/${action}`, body)
 	}
 
-	getStates(uuid){
+	getStates(uuid) {
 		return this.api.get(`/${this.moduleBase}/${uuid}/states`)
 	}
 }
