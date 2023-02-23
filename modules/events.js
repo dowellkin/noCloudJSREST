848608938de479f2f8749331d4979d0d71e2601c
oigen88 <oigen88@159-142-57-86.cisco-wifi.beltelecom.by>
2023-02-23 14:32:48 +0300

    fix ume

--- modules/events.js
+++ modules/events.js
@@ -15,9 +15,12 @@ class Events{
     cancel(type,id,uuid){
         return this.api.post('/'+this.moduleBase+'/cancel',{type,uuid,id})
     }
-    
+
     consume(type,uuid){
-        return this.api.get(`/${this.moduleBase}/sub/${type}/${uuid}`)
+        return new WebSocket(
+            this.api.axios.defaults.baseURL.replace('https','ws')+`bus/sub/${type}/${uuid}/`,
+            ['Bearer',this.api.axios.defaults.headers.common['Authorization'].split(' ')[1]]
+        )
     }
 }
 
