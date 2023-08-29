a9100aa69aea7538348b027f7de869ec31ba0208
PavelSazukin <sazukinpavelal@gmail.com>
2023-08-29 15:11:17 +0400

    fix reports methods

--- modules/reports.js
+++ modules/reports.js
@@ -4,24 +4,56 @@ class Reports {
     this.moduleBase = "billing";
   }
 
-  list({ instanceUuid, field, sort, page, limit, from, to }) {
-    return this.api.get(
-      `/${this.moduleBase}/reports${instanceUuid ? `/${instanceUuid}` : ""}`,
-      {
-        params: { sort, field, page, limit, from, to },
-      }
-    );
+  list({ field, sort, page, limit, from, to, filters }) {
+    return this.api.post(`/${this.moduleBase}/reports`, {
+      sort,
+      field,
+      page,
+      limit,
+      from,
+      to,
+      filters,
+    });
   }
 
-  count({ instanceUuid, field, sort, page, limit, from, to }) {
-    return this.api.get(
-      `/${this.moduleBase}/count/reports${
-        instanceUuid ? `/${instanceUuid}` : ""
-      }`,
-      {
-        params: { sort, field, page, limit, from, to },
-      }
-    );
+  count({ field, sort, page, limit, from, to, filters }) {
+    return this.api.post(`/${this.moduleBase}/count/reports`, {
+      sort,
+      field,
+      page,
+      limit,
+      from,
+      to,
+      filters,
+    });
+  }
+
+  base_list({ field, sort, page, limit, from, to, filters }) {
+    return this.api.get(`/${this.moduleBase}/base_reports`, {
+      params: {
+        sort,
+        field,
+        page,
+        limit,
+        from,
+        to,
+        filters,
+      },
+    });
+  }
+
+  base_count({ field, sort, page, limit, from, to, filters }) {
+    return this.api.get(`/${this.moduleBase}/count/base_reports`, {
+      params: {
+        sort,
+        field,
+        page,
+        limit,
+        from,
+        to,
+        filters,
+      },
+    });
   }
 }
 
